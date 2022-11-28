6fbacc9665b38fa792957db2c094924a4292b841
Tradelal <118475228+Tradelal@users.noreply.github.com>
2022-11-28 12:16:43 +0530

    Update ChatRoom.jsx

--- src/pages/ChatRoom/ChatRoom.jsx
+++ src/pages/ChatRoom/ChatRoom.jsx
@@ -9,6 +9,15 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
+firebase.initializeApp({
+    apiKey: "AIzaSyBTPfNjWHZRcw3TvwfbMHAdhTEZLTVpviQ",
+    authDomain: "storagedemo-c3d2d.firebaseapp.com",
+    databaseURL: "https://storagedemo-c3d2d-default-rtdb.firebaseio.com",
+    projectId: "storagedemo-c3d2d",
+    storageBucket: "storagedemo-c3d2d.appspot.com",
+    messagingSenderId: "885128275945",
+    appId: "1:885128275945:web:dbb4b15829dd3b84d50d78"
+})
 
 
 const firestore = firebase.firestore();
